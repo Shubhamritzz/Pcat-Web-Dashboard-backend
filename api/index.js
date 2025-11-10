aa00243031c35b675e103c7991a0dc9d97d37d26
Shubhamritzz <shubham@rittzdigital.com>
2025-11-10 21:07:02 +0530

    ...

--- api/index.js
+++ api/index.js
@@ -1,8 +1,8 @@
 import serverless from "serverless-http";
-import app from "../src/index.js";   
+import app from "../src/index.js";
 
 export const config = {
-  runtime: "nodejs20.x",
+  runtime: "nodejs"
 };
 
 export default serverless(app);
